e8c2b8de9004740a673f16135117a69417d328da
Garrett HE <garrett.he@outlook.com>
2024-01-16 10:36:56 +0800

    Rewrite command-line interface with click

--- aliros/__main__.py
+++ aliros/__main__.py
@@ -1,58 +1,25 @@
-import sys
-import os
-import importlib
-import argparse
-import configparser
-from aliyunsdkcore import client
+import click
+from aliros import __version__
+from aliros.commands import command_group
 
 
-def print_help():
-    usage = '''usage: aliros <command> [parameters]
-To see help text, you can run:
+def print_version(ctx: click.Context, _, value: str):
+    if not value or ctx.resilient_parsing:
+        return
 
-  aliros <command> --help
-aliros: error: the following arguments are required: command\n'''
-    sys.stderr.write(usage)
+    click.echo(__version__)
+    ctx.exit()
 
 
-def main():
-    if len(sys.argv) < 2 or sys.argv[1][0] == '-':
-        print_help()
-        return -1
+@click.group(commands=command_group)
+@click.option('--version', help='Show version information.', is_flag=True, callback=print_version, expose_value=False,
+              is_eager=True)
+@click.pass_context
+def cli(ctx: click.Context):
+    """A command-line tool to organize resources by Resource Orchestration Service for Alibaba Cloud."""
 
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('command')
-
-    # add common arguments
-    group = parser.add_argument_group('common')
-
-    group.add_argument('--region', metavar='string', help='region to use, overrides config/env settings', required=False, default=os.getenv('ALICLOUD_REGION'))
-    group.add_argument('--profile', metavar='string', help='specific profile from your credential file', required=False, default=os.getenv('ALICLOUD_PROFILE') or 'default')
-
-    try:
-        command = importlib.import_module('aliros.command.%s' % sys.argv[1].replace('-', '_'))
-    except ImportError:
-        sys.stderr.write('command "%s" not found.\n' % sys.argv[1])
-        return -1
-
-    command.add_command_arguments(parser)
-    args = parser.parse_args()
-
-    region = args.region
-    profile = args.profile
-
-    config = configparser.ConfigParser()
-    if region is None:
-        config.read(os.getenv('HOME') + '/.aliros/config')
-        region = config.get(profile, 'region')
-
-    config.read(os.getenv('HOME') + '/.aliros/credentials')
-    access_key_id = config.get(profile, 'alicloud_access_key_id')
-    secret_access_key = config.get(profile, 'alicloud_secret_access_key')
-
-    return command.execute(args=args, client=client.AcsClient(access_key_id, secret_access_key, region))
+    ctx.ensure_object(dict)
 
 
 if __name__ == '__main__':
-    main()
+    cli()
