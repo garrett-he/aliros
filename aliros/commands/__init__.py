07756cc23d906bb5194dc70e463d948fe59a03fd
Garrett HE <garrett.he@outlook.com>
2024-01-18 12:16:31 +0800

    Migrate command describe-resource-type-template

--- aliros/commands/__init__.py
+++ aliros/commands/__init__.py
@@ -2,10 +2,12 @@ from .abandon_stack import abandon_stack_command
 from .create_stack import create_stack_command
 from .delete_stack import delete_stack_command
 from .describe_resource_type import describe_resource_type_command
+from .describe_resource_type_template import describe_resource_type_template_command
 
 command_group = [
     abandon_stack_command,
     create_stack_command,
     delete_stack_command,
     describe_resource_type_command,
+    describe_resource_type_template_command,
 ]
---
+++ aliros/commands/describe_resource_type_template.py
@@ -0,0 +1,23 @@
+import json
+from aliyunsdkros.request.v20150901.DescribeResourceTypeTemplateRequest import DescribeResourceTypeTemplateRequest
+
+import click
+
+
+@click.command('describe-resource-type-template')
+@click.option('--type-name', help='Name of resource type.', required=True)
+def describe_resource_type_template_command(ctx: click.Context, type_name: str):
+    """Describe details of the specified resource type template."""
+
+    asc_client = ctx.obj['asc_client']
+    request = DescribeResourceTypeTemplateRequest()
+
+    request.set_TypeName(type_name)
+
+    status, headers, body = asc_client.get_response(request)
+
+    if 200 <= status < 300:
+        print(json.loads(body))
+        return 0
+    else:
+        raise Exception('Unexpected errors: status=%d, error=%s' % (status, body))
