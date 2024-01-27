ccbb313ac40cf1016940927fb68b5fa7569af700
Garrett HE <garrett.he@outlook.com>
2024-01-27 11:29:33 +0800

    Refactor command: create-stack

--- src/aliros/commands/create_stack.py
+++ src/aliros/commands/create_stack.py
@@ -1,10 +1,10 @@
 import json
 
 import click
-from aliyunsdkros.request.v20150901.CreateStacksRequest import CreateStacksRequest
+from aliyunsdkros.request.v20190910.CreateStackRequest import CreateStackRequest
 
 from aliros.template import YamlTemplate
-from aliros.stack import send_request
+from aliros.alicloud import send_request
 
 
 @click.command('create-stack')
@@ -13,6 +13,7 @@ from aliros.stack import send_request
 @click.option('--parameters-file', help='URL of parameters file.', required=False, type=click.Path(exists=True, dir_okay=False))
 @click.option('--timeout-mins', help='Minutes to timeout.', type=int, default=60)
 @click.option('--disable-rollback', help='Disable rollback if failed.', required=False, is_flag=True, default=False)
+@click.pass_context
 def create_stack_command(ctx: click.Context, stack_name: str, template_file: str, parameters_file: str, timeout_mins: int, disable_rollback: bool):
     """Create a new stack."""
 
@@ -21,20 +22,15 @@ def create_stack_command(ctx: click.Context, stack_name: str, template_file: str
     template = YamlTemplate()
     template.load(template_file)
 
-    body = {
-        'Name': stack_name,
-        'Template': json.dumps(template.content),
-        'TimeoutMins': timeout_mins,
-        'DisableRollback': disable_rollback
-    }
-
     if parameters_file is not None:
         parameters = YamlTemplate()
         parameters.load(parameters_file)
-        body['Parameters'] = parameters.content
+        template.content['Parameters'] = parameters.content
 
-    request = CreateStacksRequest()
-    request.set_content(json.dumps(body))
-    request.set_content_type('application/json')
+    request = CreateStackRequest()
+    request.set_StackName(stack_name)
+    request.set_TemplateBody(json.dumps(template.content))
+    request.set_TimeoutInMinutes(timeout_mins)
+    request.set_DisableRollback(disable_rollback)
 
     send_request(acs_client, request)
