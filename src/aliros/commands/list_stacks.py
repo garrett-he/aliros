7323a41e14445f364f46b730d79c431d63dc3fa9
Garrett HE <garrett.he@outlook.com>
2024-01-27 12:58:39 +0800

    Refactor command: list-stacks

--- src/aliros/commands/list_stacks.py
+++ src/aliros/commands/list_stacks.py
@@ -1,27 +1,27 @@
-import click
-from aliyunsdkros.request.v20150901.DescribeStacksRequest import DescribeStacksRequest
+from typing import List, Optional
 
-from aliros.stack import send_request
+import click
+from aliyunsdkros.request.v20190910.ListStacksRequest import ListStacksRequest
+
+from aliros.alicloud import send_request, dump_response
+from aliros.types import ListParamType
 
 
 @click.command('list-stacks')
-@click.option('--stack-id', help='ID of stack.', required=True)
-@click.option('--name', help='Name of stack.', required=True)
-@click.option('--status', help='Status of stack.', required=True)
+@click.option('--stack-names', help='Names of stack.', type=ListParamType(length=5, item_type=str), required=False)
+@click.option('--status', help='Status of stack.', type=ListParamType(length=5, item_type=str), required=False)
 @click.option('--page-number', help='Number of page.', type=int, default=1, required=False)
 @click.option('--page-size', help='Size of pages.', type=int, default=10, required=False)
-def list_stacks_command(ctx: click.Context, stack_id: str, name: str, status: str, page_number: int, page_size: int):
+@click.pass_context
+def list_stacks_command(ctx: click.Context, stack_names: Optional[List[str]], status: Optional[List[str]], page_number: int, page_size: int):
     """List stacks."""
 
     acs_client = ctx.obj['acs_client']
 
-    request = DescribeStacksRequest()
+    request = ListStacksRequest()
 
-    if stack_id is not None:
-        request.set_StackId(stack_id)
-
-    if name is not None:
-        request.set_Name(name)
+    if stack_names is not None:
+        request.set_StackName(stack_names)
 
     if status is not None:
         request.set_Status(status)
@@ -29,4 +29,4 @@ def list_stacks_command(ctx: click.Context, stack_id: str, name: str, status: st
     request.set_PageNumber(page_number)
     request.set_PageSize(page_size)
 
-    send_request(acs_client, request)
+    dump_response(send_request(acs_client, request))
