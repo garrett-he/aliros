499c52856491e9cc5785917c4a6a55009736ac08
Garrett HE <garrett.he@outlook.com>
2024-01-27 13:09:11 +0800

    Refactor command: delete-stack

--- src/aliros/commands/delete_stack.py
+++ src/aliros/commands/delete_stack.py
@@ -1,18 +1,18 @@
 import click
-from aliyunsdkros.request.v20150901.DeleteStackRequest import DeleteStackRequest
+from aliyunsdkros.request.v20190910.DeleteStackRequest import DeleteStackRequest
 
-from aliros.stack import find_stack_id, send_request
+from aliros.alicloud import send_request
 
 
 @click.command('delete-stack')
-@click.option('--stack-name', help='Name of stack.', required=True)
-def delete_stack_command(ctx: click.Context, stack_name: str):
+@click.option('--stack-id', help='Stack ID', required=True)
+@click.pass_context
+def delete_stack_command(ctx: click.Context, stack_id: str):
     """Delete the specified stack."""
 
     acs_client = ctx.obj['acs_client']
-    request = DeleteStackRequest()
 
-    request.set_StackName(stack_name)
-    request.set_StackId(find_stack_id(acs_client, stack_name))
+    request = DeleteStackRequest()
+    request.set_StackId(stack_id)
 
     send_request(acs_client, request)
